da9da87ea12f7dd82b0d51ff6190b0f90bb3704f
Byun Dong Hyun <50386280+DongHyunByun@users.noreply.github.com>
2020-04-22 16:22:58 +0900

    Add files via upload

---
+++ lab1/p4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	cout << "Please enter two integer values: " << endl;
+	cout << "A : ";
+	cin >> B ;
+	cout << "B : ";
+	cin >> A;
+
+	cout << "value of A is : " << A << endl;
+	cout << "value of B is : " << B << endl;
+
+	return 0;
+}
---
+++ lab1/p5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int fah;
+	cout << "Please enter Fahrenheit value: "; cin >> fah;
+	cout << "Celsius value is " << 5 / 9 * (fah - 32);
+	return 0;
+
+}
